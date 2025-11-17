0a68bfd83c9fc1327f3d6c3679b2f8d0e36aa85d
Sreesai969 <sreesairegatti@gmail.com>
2025-11-17 11:26:24 +0530

    operator.html

---
+++ day3/myscript.js
@@ -0,0 +1,7 @@
+console.log("external file")
+let parent_ele=document.create.element("div")
+document.body.appendChild(parent_ele)
+let para_ele=document.create.element("paragraph")
+para_ele.textcontent="paragraph"
+let head_ele=document.create.element("heading")
+head_ele.innerHTML="heading"
